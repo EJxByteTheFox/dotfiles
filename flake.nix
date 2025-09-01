4c0668d140d636f2276bc9b40530aeb2dc612a1f
Ellie Johnston (Byte the Fox) <155034127+EJxByteTheFox@users.noreply.github.com>
2025-09-01 18:28:11 -0400

    Fixes XVI

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs";
 
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
